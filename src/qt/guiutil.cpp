7598a2efd61153fbde5455b2e3e45e5f8530a021
infinitecoin-core <59826757+infinitecoin-core@users.noreply.github.com>
2021-04-08 12:14:07 +0800

    Add files via upload

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -106,7 +106,7 @@ void setupAddressWidget(QValidatedLineEdit *widget, QWidget *parent)
 #if QT_VERSION >= 0x040700
     // We don't want translators to use own addresses in translations
     // and this is the only place, where this address is supplied.
-    widget->setPlaceholderText(QObject::tr("Enter a Infinitecoin address (e.g. %1)").arg("Ler4HNAEfwYhBmGXcFP2Po1NpRUEiK8km2"));
+    widget->setPlaceholderText(QObject::tr("Enter a Infinitecoin address (e.g. %1)").arg("iAQbpnpKn8MDdcDQMUdA3GheoH4NwEUWPH"));
 #endif
     widget->setValidator(new BitcoinAddressEntryValidator(parent));
     widget->setCheckValidator(new BitcoinAddressCheckValidator(parent));
